88a731e329033e9862cb36342321bc662c8eaa66
s6xybr8in <s6xybr8in@gmail.com>
2023-08-03 01:08:55 +0900

    FIRST COMMIT11

---
+++ App/lib/user.dart
@@ -0,0 +1,7 @@
+class User_Model{
+  final String name;
+  final String id;
+  final String password;
+  final String nickname;
+  User_Model({ required this.name,required this.nickname,required this.id,required this.password});
+}
