05150074ba1aaecfd33f16647bc0deb64595bf80
Chaesh <58378989+Chaesh156@users.noreply.github.com>
2023-08-15 23:42:49 +0900

    add Back Events and PostPage

---
+++ kwanho/lib/Screens/reportPage.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:test1/OpenPost.dart';
+
+class ReportPage extends StatefulWidget{
+  const ReportPage({Key? key}) : super(key: key);
+
+  @override
+  State<ReportPage> createState() => _ReportPageState();
+}
+
+
+class _ReportPageState extends State<ReportPage> {
+  List<String> _reportType = ['선택','욕설','비방','음란','허위','기타'];
+  String _reportDefault = '선택';
+
+  Future<bool> _onBackPressed() async {
+    return await showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("나가면 지금까지 작성한 내용은 사라집니다."),
+            actions: <Widget>[
+              TextButton(onPressed: () {Navigator.pop(context,true);},
+                child: Text("나가기"),),
+              ElevatedButton(onPressed: () {Navigator.pop(context,false);},
+                child: Text("취소"),)
+            ],
+          );
+        }
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        onWillPop: _onBackPressed,
+        child: Scaffold(
+        appBar: AppBar( //상단 바
+          leading: BackButton(
+              onPressed: (){
+                _onBackPressed();}
+          ),
+          title: Text("신고 - 게시물 제목",
+            style: TextStyle(fontFamily: 'Aggro',fontSize: 30),),
+          backgroundColor: const Color(0xffb3e5fc),
+        ),
+        backgroundColor: const Color(0xffffffff),
+        body:Container(
+          decoration: BoxDecoration(
+            color: const Color(0xffffffff),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
+                  child: SizedBox(
+                    child: Column(
+                      children: [
+                        Align(alignment: Alignment.centerLeft,child: Text("신고 사유"),),
+                        Container(
+                            width: double.infinity,
+                            child: ButtonTheme(
+                              alignedDropdown: true,
+                              child: DropdownButton<String>(
+                                  value: _reportDefault,
+                                  items: _reportType.map((String item) {
+                                    return DropdownMenuItem<String>(
+                                      child: Text("$item"),
+                                      value: item,);
+                                  }).toList(),
+                                  isExpanded: true,
+                                  onChanged: (dynamic value){
+                                    setState(() {
+                                      _reportDefault = value;
+                                    });
+                                  }),))
+                      ],
+                    ),
+                  )), //신고 사유
+              Padding(padding: EdgeInsets.fromLTRB(20, 10, 20, 10), //신고 내용
+                child: SizedBox(
+                  child: Column(
+                    children: [
+                      Align(alignment: Alignment.centerLeft,child: Text("신고 내용")),
+                      TextField()
+                    ],
+                  ),
+                ),),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                    child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                            side: BorderSide(color: Color(0xffdc3545)),
+                            foregroundColor: Color(0xffdc3545)),
+                        onPressed: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => OpenPost()));
+                        },
+                        child: Text(
+                          '신고',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: const Color(0xffdc3545),
+                          ),
+                          softWrap: false,
+                        )),),
+                ],
+              )
+            ],
+          ),
+        )
+    ));
+  }
+}
