2852e41fd08b31723ae74e3d050f55abdb7bac3b
Chaesh <58378989+Chaesh156@users.noreply.github.com>
2023-08-15 21:08:36 +0900

    08/15 add comment and revise page

--- kwanho/OpenPost.dart
+++ kwanho/OpenPost.dart
@@ -1,17 +1,234 @@
 import 'package:flutter/material.dart';
-import 'package:adobe_xd/pinned.dart';
-import './like.dart';
-import './report.dart';
-import './ReportPage.dart';
-import 'package:adobe_xd/page_link.dart';
+import 'reportPage.dart';
+
 
 class OpenPost extends StatelessWidget {
-  //MainPage({Key key,}) : super(key: key);
+  //OpenPost({Key key,}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xffffffff),
-      body: Stack(
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Aggro'),
+        home: Scaffold(
+          appBar: AppBar( //상단 바
+            leading: BackButton(),
+            title: Text("게시판 종류",
+            style: TextStyle(fontFamily: 'Aggro',fontSize: 30),),
+            backgroundColor: Color(0xffb3e5fc),
+          ),
+          backgroundColor: Color(0xffffffff),
+          body:
+          ListView(
+            padding: EdgeInsets.all(5),
+              children: [
+                Column(
+                    children: [
+                      Container(
+                        constraints: BoxConstraints(
+                          minHeight: 150
+                        ),
+                        width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Color(0xffffffff),
+                            border: Border.all(color: Colors.black,width: 0.5),
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(4.0),
+                              topRight: Radius.circular(4.0),
+                            bottomLeft: Radius.circular(4.0),
+                            bottomRight: Radius.circular(4.0)),
+                          ),
+                          child: Expanded(
+                            child: Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.fromLTRB(8, 3, 8, 3),
+                                child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,//글 제목, 작성자 박스
+                                children: const [
+                                  SizedBox(
+                                    height: 50,
+                                    child: Text("글 제목",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        color: Color(0xff000000),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    child: Text("작성자 - 나이대",
+                                      style: TextStyle(
+                                        fontSize: 13,
+                                        color: Color(0xff6c757d),
+                                      ),
+                                      textAlign: TextAlign.right,
+                                      softWrap: false,),
+                                  )
+                                ],
+                              ),),
+                              Padding(
+                                padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
+                                child:  Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Expanded(
+                                    child: Text("Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem ",
+                                      textAlign: TextAlign.left,
+                                      softWrap: true,),
+                                  ),),
+                              ),//본문 내용
+                              Padding(
+                                  padding: EdgeInsets.fromLTRB(5, 30, 5, 5),
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,//좋아요, 신고 버튼 박스
+                                    children: [
+                                      OutlinedButton(
+                                        style: OutlinedButton.styleFrom(
+                                          side: BorderSide(color: Color(0xff007bff)),
+                                            foregroundColor: Color(0xff007bff)),
+                                          onPressed: (){},
+                                          child: Text(
+                                        '좋아요',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          color: Color(0xff007bff),
+                                        ),
+                                        softWrap: false,
+                                      )),
+                                      OutlinedButton(
+                                        style: OutlinedButton.styleFrom(
+                                          side: BorderSide(color: Color(0xffdc3545)),
+                                            foregroundColor: Color(0xffdc3545)),
+                                          onPressed: (){
+                                          Navigator.push(context, MaterialPageRoute(builder: (context) => reportPage()));
+                                        },
+                                          child: Text(
+                                            '신고',
+                                            textAlign: TextAlign.center,
+                                            style: TextStyle(
+                                              fontSize: 16,
+                                              color: const Color(0xffdc3545),
+                                            ),
+                                            softWrap: false,
+                                          )),
+                                    ],
+                                  ),)
+                            ],
+                          ),)
+                      ),
+                      //댓글
+                      Container(
+                          decoration: BoxDecoration(
+                            border: Border(bottom: BorderSide(color: Colors.black, width: 1))
+                          ),
+                          child:
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(10, 10, 10, 1),
+                            child: Align(alignment: Alignment.centerLeft,
+                              child: Text("댓글",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    color: Color(0xff707070),
+                                  )
+                              ),
+                            ),
+                          )
+
+                      ),
+                      Padding(//세부 댓글
+                        padding: EdgeInsets.fromLTRB(1, 5.5, 5.5, 1),
+                        child: Container(
+                            decoration: BoxDecoration(
+                            border: Border(bottom: BorderSide(color: Colors.black, width: 0.5))
+                        ),
+                            width: double.infinity,
+                            child: Align(alignment: Alignment.centerLeft,
+                              child: Expanded(
+                                  child: Column(
+                                    children: const [
+                                      Text("댓글 내용",softWrap: true,
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            color: Color(0xff000000),
+                                            height: 1.5,
+                                          )),
+                                      Padding(
+                                        padding: EdgeInsets.fromLTRB(10, 1, 1, 3),
+                                        child: Text("작성자 - 나이대",
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          color: Color(0xff6c757d),
+                                        ),),)
+                                    ],
+                                  )),
+                            )
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(1, 5.5, 5.5, 1),
+                        child: Container(
+                            decoration: BoxDecoration(
+                                border: Border(bottom: BorderSide(color: Colors.black, width: 0.5))
+                            ),
+                            width: double.infinity,
+                            child: Align(alignment: Alignment.centerLeft,
+                              child: Expanded(
+                                  child: Column(
+                                    children: const [
+                                      Text("댓글 내용",softWrap: true,
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            color: Color(0xff000000),
+                                            height: 1.5,
+                                          )),
+                                      Padding(
+                                        padding: EdgeInsets.fromLTRB(10, 1, 1, 3),
+                                        child: Text("작성자 - 나이대",
+                                          style: TextStyle(
+                                            fontSize: 12,
+                                            color: Color(0xff6c757d),
+                                          ),),)
+                                    ],
+                                  )),
+                            )
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(1, 5.5, 5.5, 1),
+                        child: Container(
+                            decoration: BoxDecoration(
+                                border: Border(bottom: BorderSide(color: Colors.black, width: 0.5))
+                            ),
+                            width: double.infinity,
+                            child: Align(alignment: Alignment.centerLeft,
+                              child: Expanded(
+                                  child: Column(
+                                    children: const [
+                                      Text("댓글 내용",softWrap: true,
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            color: Color(0xff000000),
+                                            height: 1.5,
+                                          )),
+                                      Padding(
+                                        padding: EdgeInsets.fromLTRB(10, 1, 1, 3),
+                                        child: Text("작성자 - 나이대",
+                                          style: TextStyle(
+                                            fontSize: 12,
+                                            color: Color(0xff6c757d),
+                                          ),),)
+                                    ],
+                                  )),
+                            )
+                        ),
+                      ), //댓글
+                    ]
+                )
+              ]
+          )
+          )
+    );
+  }
+}
+      /*Stack(
         children: <Widget>[
           Pinned.fromPins(
             Pin(start: 0.0, end: 0.0),
@@ -405,36 +622,7 @@ class OpenPost extends StatelessWidget {
               softWrap: false,
             ),
           ),
-          Pinned.fromPins(
-            Pin(start: 0.0, end: 0.0),
-            Pin(size: 57.0, start: 0.0),
-            child: Stack(
-              children: <Widget>[
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color(0xffb3e5fc),
-                    border:
-                        Border.all(width: 1.0, color: const Color(0xff707070)),
-                  ),
-                ),
-                Pinned.fromPins(
-                  Pin(size: 79.0, start: 62.0),
-                  Pin(size: 20.0, middle: 0.5135),
-                  child: Text(
-                    '게시판 종류',
-                    style: TextStyle(
-                      fontFamily: 'Segoe UI',
-                      fontSize: 15,
-                      color: const Color(0xff000000),
-                      fontWeight: FontWeight.w700,
-                    ),
-                    softWrap: false,
-                  ),
-                ),
-                Container(),
-              ],
-            ),
-          ),
+
           Pinned.fromPins(
             Pin(start: 5.0, end: 5.0),
             Pin(size: 419.0, start: 64.0),
@@ -528,7 +716,5 @@ class OpenPost extends StatelessWidget {
             ),
           ),
         ],
-      ),
-    );
-  }
-}
+      ),*/
+
